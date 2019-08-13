56e1bf8b401d21175464039af04b8553fa6ba6e1
Steven Montes <cmontesa@ucenfotec.ac.cr>
2019-08-12 19:47:43 -0600

    Agrega codigo de lista de arcos

--- Logica/LogicaGrafo/ListaArco.cs
+++ Logica/LogicaGrafo/ListaArco.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Logica.LogicaGrafo
+﻿namespace Logica.LogicaGrafo
 {
     public class ListaArco
     {
+        public Arco Cabeza { get; set; }
+
+        public ListaArco()
+        {
+            Cabeza = null;
+        }
+
+        public bool Insertar(Arco nArco)
+        {
+            var insertado = false;
+
+            if(nArco != null)
+            {
+                if (!EsVacio())
+                {
+                    nArco.Siguiente = Cabeza;
+                }
+
+                Cabeza = nArco;
+                insertado = true;
+            }
+
+            return insertado;
+        }
+
+        public Arco Buscar(Vertice destino)
+        {
+            var aux = Cabeza;
+            Arco encontrado = null;
+
+            while(aux != null)
+            {
+                if (aux.VerticeDestino.Nombre.Equals(destino.Nombre))
+                {
+                    encontrado = aux;
+                    break;
+                }
+
+                aux = aux.Siguiente;
+            }
+
+            return encontrado;
+        }
+
+        private bool EsVacio()
+        {
+            var esVacio = false;
+
+            if(Cabeza == null)
+            {
+                esVacio = true;
+            }
+
+            return esVacio;
+        }
     }
 }

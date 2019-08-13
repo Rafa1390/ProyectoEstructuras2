2544e4c45671b3e83046b03682bca0b23b491492
Steven Montes <cmontesa@ucenfotec.ac.cr>
2019-08-12 19:40:52 -0600

    Agrega proyecto back end
    Agrega clases de los grafos, vertices y arcos.

---
+++ Logica/LogicaGrafo/ListaVertice.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.LogicaGrafo
+{
+    public class ListaVertice
+    {
+        public Vertice Cabeza { get; set; }
+
+        public ListaVertice()
+        {
+            Cabeza = null;
+        }
+
+        /// <summary>
+        /// Ingresa un nuevo vertice a la lista de vertices.
+        /// </summary>
+        /// <param name="nVertice"></param>
+        /// <returns></returns>
+        public bool Insertar(Vertice nVertice)
+        {
+            var insertado = false;
+
+            if(nVertice != null)
+            {
+                insertado = true;
+
+                if (!EsVacio())
+                {
+                    nVertice.Siguiente = Cabeza;
+                }
+
+                Cabeza = nVertice;
+            }
+
+            return insertado;
+        }
+
+        /// <summary>
+        /// Responsable de buscar un vertice en especifico.
+        /// </summary>
+        /// <param name="nNombre"></param>
+        /// <returns></returns>
+        public Vertice Buscar(string nNombre)
+        {
+            var aux = Cabeza;
+            Vertice encontrado = null;
+
+            while(aux != null)
+            {
+                if (aux.Nombre.Equals(nNombre))
+                {
+                    encontrado = aux;
+                    break;
+                }
+
+                aux = aux.Siguiente;
+            }
+
+            return encontrado;
+        }
+
+        /// <summary>
+        /// Indica si la lista esta vacia.
+        /// </summary>
+        /// <returns></returns>
+        public bool EsVacio()
+        {
+            var esVacio = false;
+
+            if(Cabeza == null)
+            {
+                esVacio = true;
+            }
+
+            return esVacio;
+        }
+    }
+}
---
+++ Logica/LogicaGrafo/Vertice.cs
@@ -0,0 +1,23 @@
+﻿namespace Logica.LogicaGrafo
+{
+    public class Vertice
+    {
+        public string Nombre { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+        public ListaArco Arcos { get; set; }
+        public Vertice Siguiente { get; set; }
+
+        public Vertice()
+        {
+
+        }
+
+        public Vertice(string nNombre, decimal nLat, decimal nLong)
+        {
+            Nombre = nNombre;
+            Latitud = nLat;
+            Longitud = nLong;
+        }
+    }
+}
